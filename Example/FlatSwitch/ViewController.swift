ca9ec06be2c0f4d68d82ca298e4fb8749d53bc27
Hussein AlRyalat <hus.sc@aol.com>
2020-01-10 11:11:53 +0200

    disabled state and swipe gestures

--- Example/FlatSwitch/ViewController.swift
+++ Example/FlatSwitch/ViewController.swift
@@ -18,20 +18,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+		let turquise = UIColor(red: 0.10588, green: 0.73725, blue: 0.61176, alpha: 1.0)
+		let blue = UIColor(red: 0.16078, green: 0.50196, blue: 0.72549, alpha: 1.0)
+		let lightGray = UIColor(red: 0.74117, green: 0.76470, blue: 0.78039, alpha: 1.0)
+		
+		firstFlatSwitch.onConfiguration = .init(backgroundColor: turquise,
+												title: "Hola",
+												thumbBackgroundColor: turquise,
+												thumbImage: UIImage(named: "check"),
+												thumbTintColor: .white,
+												titleColor: .white)
+		firstFlatSwitch.offConfiguration = .init(backgroundColor: .white,
+												 title: "Off",
+												 thumbBackgroundColor: lightGray,
+												 thumbImage: UIImage(named: "check"),
+												 thumbTintColor: .white,
+												 titleColor: lightGray)
+		firstFlatSwitch.offBorderColor = lightGray
+		firstFlatSwitch.disabledBackgroundColor = turquise.withAlphaComponent(0.75)
+		firstFlatSwitch.labelFont = UIFont.systemFont(ofSize: 15, weight: .medium)
+		firstFlatSwitch.disabledTitle = "Disabled"
+		
+		secondFlatSwitch.onConfiguration.backgroundColor = blue
+		secondFlatSwitch.onConfiguration.thumbBackgroundColor = blue
+		secondFlatSwitch.onConfiguration.title = "Enabled"
+		secondFlatSwitch.offConfiguration.title = "Disabled"
     }
 
 	@IBAction func flatSwitchValueChanged(_ sender: FlatCheckBox) {
-		sender.set(isOn: !sender.isOn, animated: true)
 	}
 	
 	
-	@IBAction func secondFlatSwitchValueChanged(_ sender: Any) {
-		firstFlatSwitch.set(isEnabled: !firstFlatSwitch.isEnabled, animated: true)
+	@IBAction func secondFlatSwitchValueChanged(_ sender: FlatCheckBox) {
+		firstFlatSwitch.set(isEnabled: sender.isOn, animated: true)
 	}
 }
 
